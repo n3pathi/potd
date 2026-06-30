8e406aff5102dd3e2f67be92bf67b4637cd22827
Nitin Tripathi <nitin.tripathi@gmail.com>
2026-06-30 09:45:49 +0530

    refactor to improve solution to O(m)

--- app/src/main/java/g4g/MinOpsToIdenticalArray.java
+++ app/src/main/java/g4g/MinOpsToIdenticalArray.java
@@ -8,17 +8,20 @@ public class MinOpsToIdenticalArray {
     static int solve(int[] a, int[] b) {
         int n = a.length;
         int m = b.length;
-        int[][] dp = new int[n+1][m+1];
+        int[] dp = new int[m + 1];
         for (int i = 1; i <= n; i++) {
+            int prev = 0; // tracks dp[i-1][j-1]
             for (int j = 1; j <= m; j++) {
+                int temp = dp[j]; // tracks dp[i-1][j]
                 if (a[i - 1] == b[j - 1]) {
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                    dp[j] = prev + 1;
                 } else {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                    dp[j] = Math.max(dp[j], dp[j - 1]);
                 }
+                prev = temp;
             }
         }
-        int lcs = dp[n][m];
+        int lcs = dp[m];
         return (n - lcs) + (m - lcs);
     }
 }
